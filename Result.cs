01e67a2899cc5c3306cf3b34fcd9bc65ba084503
zsq1460 <61648767+zsq1460@users.noreply.github.com>
2021-09-29 12:57:31 +0800

    Add files via upload

---
+++ Result.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class Result<T>
+
+    {
+        public int Code = 1;
+        public string msg = "";
+        public T res = default(T);
+
+    public static Result Ok(T result)
+        {
+            return new Result() { res = result };
+        }
+
+        public static Result Ok(string message,T Result= default(T))
+        {
+            return new Result() { msg = message, res = Result };
+
+        }
+        public static Result Err(string message)
+        {
+            return new Result() { Code = -1 ,msg = message };
+        }
+    }
+    public class Result : Result<dynamic>
+    {
+
+    }
+}
